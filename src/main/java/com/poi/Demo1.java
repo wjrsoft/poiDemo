fc4023ff669da1bc069eb18ed455bde19a52d2bf
jinrong.wang <jinrong.wang@yeepay.com>
2018-03-15 17:01:32 +0800

    test22

--- src/main/java/com/poi/Demo1.java
+++ src/main/java/com/poi/Demo1.java
@@ -6,7 +6,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 
 public class Demo1 {
-	//Test
+	//Test  adfadfa
 	public static void main(String[] args) throws Exception {
 		Workbook wb=new HSSFWorkbook(); // 定义一个新的工作簿
 		FileOutputStream fileOut=new FileOutputStream("c:\\用Poi搞出来的工作簿.xls");
